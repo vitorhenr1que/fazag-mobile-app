8d1d224af23d103a299e869d63b0272c3b6b391c
Vitor Henrique <vhpsantos@gmail.com>
2023-01-30 10:46:02 -0300

    removido react-native-gesture

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { Header } from './src/components/Header';
 import Routes from './src/routes';
-import 'react-native-gesture-handler'
 import AuthProvider from './src/contexts/auth';
 
 export default function App() {
