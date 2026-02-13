fd88f4bc22e5c07a7cb26deb61ddb658add9fea0
Vitor Henrique <vitor@MacBookAir.home>
2026-02-13 18:45:15 -0300

    test2

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,11 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+// metro.config.js
+const { getDefaultConfig } = require('@expo/metro-config');
+
 const config = getDefaultConfig(__dirname);
 
+// SVG transformer
 config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
-config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
-config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
+config.resolver.assetExts = config.resolver.assetExts.filter((ext) => ext !== 'svg');
+config.resolver.sourceExts = [...new Set([...config.resolver.sourceExts, 'svg'])];
 
 module.exports = config;
